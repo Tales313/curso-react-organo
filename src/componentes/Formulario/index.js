27cc7ea0285df3a376dd9db2de4f71b3421dcadf
Tales313 <t4les313@gmail.com>
2022-07-30 18:06:56 -0300

    usando estado nos campos de texto e combobox

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import './Formulario.css'
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import {useState} from "react";
 
 const Formulario = () => {
 
@@ -15,6 +16,11 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
     const aoSalvar = (evento) => {
         evento.preventDefault()
         console.log('Form foi submetido')
@@ -24,10 +30,10 @@ const Formulario = () => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome"/>
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o cargo"/>
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
-                <ListaSuspensa label="Time" itens={times}/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome" valor={nome} aoAlterado={valor => setNome(valor)}/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o cargo" valor={cargo} aoAlterado={valor => setCargo(valor)}/>
+                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa label="Time" itens={times} valor={time} aoAlterado={valor => setTime(valor)}/>
                 <Botao>Criar Card</Botao>
             </form>
         </section>

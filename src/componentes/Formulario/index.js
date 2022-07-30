6e395ef5caa8141f65a3f0001160250cefa7933f
Tales313 <t4les313@gmail.com>
2022-07-30 17:20:35 -0300

    alterando comportamento do submit

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -15,12 +15,17 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome"/>
-                <CampoTexto label="Cargo" placeholder="Digite o cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o cargo"/>
                 <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem"/>
                 <ListaSuspensa label="Time" itens={times}/>
                 <Botao>Criar Card</Botao>

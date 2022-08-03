1e6f35b8b9b10e9090a348451b7398ff70ece2eb
Tales313 <t4les313@gmail.com>
2022-08-02 23:03:53 -0300

    componente de colaborador com estilo

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho">
+                <img src="https://github.com/Tales313.png" alt="Foto de Tales Araujo" />
+            </div>
+            <div className="rodape">
+                <h4>Tales</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,12 @@
 import './Time.css'
+import Colaborador from "../Colaborador";
 
 const Time = (props) => {
     return (
         <section className="time" style={{backgroundColor: props.corSecundaria}}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador/>
+            <Colaborador/>
         </section>
     )
 }

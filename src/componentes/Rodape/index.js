580392a637d32a19e3fa3183922007aab891092a
Tales313 <t4les313@gmail.com>
2022-08-04 22:35:49 -0300

    add rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,21 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className="rodape">
+            <div className="redes-sociais">
+                <img src='/imagens/fb.png' alt="Icone do facebook"/>
+                <img src='/imagens/tw.png' alt="Icone do twitter" />
+                <img src='/imagens/ig.png' alt="Icone do instagram" />
+            </div>
+            <div className="organo-icon">
+                <img src="/imagens/logo.png" alt="Logo do organo"/>
+            </div>
+            <div className="creditos">
+                Desenvolvido por Alura.
+            </div>
+        </footer>
+    )
+}
+
+export default Rodape
